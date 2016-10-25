ed26407015e5f210df7a81b5467ed17b9ff9baec
Harry Saephan <saephan215@gmail.com>
2016-10-25 13:24:50 -0400

    Added flash, sign up page, test

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ root 'static_pages#home'
 
   get '/signup', to: 'users#new'
 
+  resources :users
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
