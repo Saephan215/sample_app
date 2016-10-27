8e9b0145114e994e95534c741232fea4dfa0621d
Harry Saephan <saephan215@gmail.com>
2016-10-27 10:40:04 -0400

    Added in login features - Sessions for maintaining temporary cookies, flash on rendered pages, use session method to store id

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
 
 root 'static_pages#home'
 
   get '/about', to: 'static_pages#about'
-
-  get '/home', to: 'static_pages#home'
-
   get '/help', to: 'static_pages#help'
-
   get '/contact', to: 'static_pages#contact'
-
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
