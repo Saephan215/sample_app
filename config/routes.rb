469cf96b7a1b6918a7505fe828bb43d02c59a8ce
Harry Saephan <saephan215@gmail.com>
2016-11-03 17:36:18 -0400

    Account activations modeled as a resource, generated Action Mailers, account activations uses a generated token to create a unique URL for activating users, account activations use a hashed activation digest to securely identify valid activations requests, both mailer test and integrations test verifies the behavior of the User mailer, send emails in production using SendGrid

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   resources :users
+  resources :account_activations, only: [:edit]
 
 
 
